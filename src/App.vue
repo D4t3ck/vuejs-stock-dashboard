13880b5c75708dfbf3f98f3d6346d9fd95070a50
daniel sinteck <d.sint3ck@gmail.com>
2024-07-13 00:47:45 +0200

    ++ BaseCard;

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <!-- Dein Inhalt hier -->
-  </div>
+  <BaseCard>
+    <h1>Überschrift</h1>
+    <p>Dies ist ein Beispieltext, der in die Card-Komponente eingefügt wird.</p>
+  </BaseCard>
+
+
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
 export default {
-  name: "App",
-  components: {},
-};
+  name: 'App',
+  components: {
+    BaseCard
+  }
+}
 </script>
 
 <style>
 body {
   margin: 0;
 }
-
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  
   width: 100vw;
   min-height: 100vh;
-  background: radial-gradient(
-    71.11% 100% at 50% 0%,
-    #020204 14.6%,
-    #011f35 100%
-  );
+  padding: 100px;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
+  color: whitesmoke;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="card">
+        <!-- Slot für beliebigen Inhalt -->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard'
+}
+</script>
+
+<style scoped>
+.card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+}
+</style>
